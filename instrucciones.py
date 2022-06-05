d6c32ce119f4040b11515e3103ed14245efdb7d3
Luis Montero <virgo-2593@hotmail.com>
2022-06-05 18:31:06 -0500

    first commit

---
+++ instrucciones.py
@@ -0,0 +1,78 @@
+class Instruccion:
+    '''This is an abstract class'''
+
+class Write(Instruccion) :
+    '''
+        Esta clase representa la instrucción imprimir.
+        La instrucción imprimir únicamente tiene como parámetro una cadena
+    '''
+
+    def __init__(self,  cad) :
+        self.cad = cad
+
+class During(Instruccion) :
+    '''
+        Esta clase representa la instrucción mientras.
+        La instrucción mientras recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera.
+    '''
+
+    def __init__(self, expcomparativa, instrucciones = []) :
+        self.expcomparativa = expcomparativa
+        self.instrucciones = instrucciones
+
+class Definicion(Instruccion) :
+    '''
+        Esta clase representa la instrucción de definición de variables.
+        Recibe como parámetro el nombre del identificador a definir
+    '''
+
+    def __init__(self, id) :
+        self.id = id
+
+class Asignacion(Instruccion) :
+    '''
+        Esta clase representa la instrucción de asignación de variables
+        Recibe como parámetro el identificador a asignar y el valor que será asignado.
+    '''
+
+    def __init__(self, id, expNumerica) :
+        self.id = id
+        self.expNumerica = expNumerica
+
+class Iteof(Instruccion) :
+    '''
+        Esta clase representa la instrucción if.
+        La instrucción if recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera.
+    '''
+
+    def __init__(self, expcomparativa, instrucciones = []) :
+        self.expcomparativa = expcomparativa
+        self.instrucciones = instrucciones
+
+class Otherwise(Instruccion) :
+    '''
+        Esta clase representa la instrucción if-else.
+        La instrucción if-else recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera y otro lista de instrucciones
+        a ejecutar si la expresión lógica es falsa.
+    '''
+
+    def __init__(self, expcomparativa, instrIfVerdadero = [], instrIfFalso = []) :
+        self.expcomparativa = expcomparativa
+        self.instrIfVerdadero = instrIfVerdadero
+        self.instrIfFalso = instrIfFalso
+
+
+class Sasto(Instruccion):
+    '''
+           Esta clase representa la instrucción for.
+           La instrucción if-else recibe como parámetro una expresión lógica y la lista
+           de instrucciones a ejecutar si la expresión lógica es verdadera y otro lista de instrucciones
+           a ejecutar si la expresión lógica es falsa.
+       '''
+    def __init__(self, expcomparativa,expNumerica, instrucciones = []) :
+        self.expcomparativa = expcomparativa
+        self.expNumerica=expNumerica
+        self.instrucciones = instrucciones
